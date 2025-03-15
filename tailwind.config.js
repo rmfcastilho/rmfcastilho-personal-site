ef3d795b2f8d878eb9ef7bd0e5ab19a58e8d661f
Rodrigo M. F. Castilho <rodrigo.m.castilho@outlook.com>
2025-03-15 13:03:25 -0300

    chore: add base files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
+// tailwind.config.js
+const {heroui} = require("@heroui/react");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    `./src/pages/**/*.{js,jsx,ts,tsx}`,
-    `./src/components/**/*.{js,jsx,ts,tsx}`,
+    // ...
+    // make sure it's pointing to the ROOT node_module
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  darkMode: "class",
+  plugins: [heroui()],
+};
